b69e0653cc12f8492ccc144dda0e2bd1caf3c6b1
Adrian Anaya <aanaya79@cnm.edu>
2020-10-22 11:09:27 -0600

    Parsing from stdIn, writing to stdOut.

--- src/edu/cnm/deepdive/Parser.java
+++ src/edu/cnm/deepdive/Parser.java
@@ -9,20 +9,20 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.stream.Collectors;
 
 public class Parser {
 
-  private static final String TEST_SCHEMA_FILE = "test-schema.json";
+  private static final String STATEMENT_TERMINATOR = ";";
+  private static final String STATEMENT_SEPARATOR = String.format("%s%n%n", STATEMENT_TERMINATOR);
 
   public static void main(String... args) throws IOException {
-    try (InputStream input = new FileInputStream(TEST_SCHEMA_FILE)) {
-      Parser parser = new Parser();
-      Schema schema = parser.parse(input);
-      System.out.println(parser.getDdl(schema));
-    }
-     // Open file as InputStream
-     // Parse schema
-     // Print ddl string
+    Parser parser = new Parser();
+    Schema schema = parser.parse(System.in);
+    System.out.println(parser.getDdl(schema));
+    // Open file as InputStream
+    // Parse schema
+    // Print ddl string
   }
 
   private Schema parse(InputStream input) throws IOException {
@@ -38,7 +38,8 @@ public class Parser {
 
   private String getDdl(Schema schema) {
     // Assemble ddl from objects read by Gson.
-    return null;
+    return schema.getDdl()
+        .collect(Collectors.joining(STATEMENT_SEPARATOR, "", STATEMENT_TERMINATOR));
   }
 
 }
